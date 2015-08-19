83c2badb40ab2ceb0e6c485064b84ab394c371ce
Gustavo Morfim <gustavomorfim@hotmail.com>
2015-08-19 20:43:37 -0300

    comentario gustavo

--- BancoIdeias/src/bancoideias/BancoIdeias.java
+++ BancoIdeias/src/bancoideias/BancoIdeias.java
@@ -16,7 +16,7 @@ public class BancoIdeias {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //as
+        // teste Gustavo
     }
     
 }
