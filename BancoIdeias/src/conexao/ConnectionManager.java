2c50ed24156b73d23f5b9f214abd3307b7e21c24
Programação <Programao@172.20.116.183>
2015-08-25 20:28:43 -0300

    Ederley

---
+++ BancoIdeias/src/conexao/ConnectionManager.java
@@ -0,0 +1,26 @@
+package conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionManager {
+
+    public static Connection getConnection() {
+
+        Connection conn = null;
+
+        String STR_DRIVER = "org.gjt.mm.mysql.Driver";
+        String STR_CONEX = "jdbc:mysql://localhost:3306/DBBANCODEIDEIAS";
+        String USER = "root";
+        String PASSWORD = "";
+
+        try {
+            Class.forName(STR_DRIVER);
+            conn = DriverManager.getConnection(STR_CONEX, USER, PASSWORD);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return conn;
+    }
+}
