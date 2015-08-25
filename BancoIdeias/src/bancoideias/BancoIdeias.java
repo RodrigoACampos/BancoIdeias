ed0005ae7f9d0fac31cf67d968171c21914cfc52
LUCAS <LUCAS@172.20.116.88>
2015-08-25 19:50:07 -0300

    Tentando atualizar aqui

--- BancoIdeias/src/bancoideias/BancoIdeias.java
+++ BancoIdeias/src/bancoideias/BancoIdeias.java
@@ -21,24 +21,7 @@ public class BancoIdeias {
         JanelaTeste Jan = new JanelaTeste();
         Jan.setVisible(true);
         // TODO code application logic here
-        //ALAHU AKBAR
-        //JUCABECINHA_ROXA
-        //TESTE 123
-        //CUCA BELUDO
-        //PAULA TEJANO
-        //DORALINO REGO
-        //CREMOSA SANTOS
-        //CHUP CHUP DE UVA]
-        //PAULA DENTRO
-        //PAULA FORA
-        //FILHA DO HASSUM
-        //GOSTOSA PRA CHUCHU
-        //PROIBIDA1 DU FUNK
-        //MATEI_O_BALAN_NA_FAQUINHA
-        //OK_OK_O_BALAN_ME_DEU_UNS_HEADSHOTS_DE_DEAGLE
-        //TEST MARCELO
-        //rodrigo
-        //LucasFurlani
+       
         String teste2;
         String nome1;
     }
