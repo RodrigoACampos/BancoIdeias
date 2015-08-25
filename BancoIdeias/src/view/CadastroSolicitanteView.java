faac02e42de791a730d71781208776cd88864768
joao_petri <joao_petri@SNSOO1EP01VM119.saojose.sc.senai.br>
2015-08-25 20:08:33 -0300

    Atualização Solicitante View

--- BancoIdeias/src/view/CadastroSolicitanteView.java
+++ BancoIdeias/src/view/CadastroSolicitanteView.java
@@ -30,8 +30,8 @@ public class CadastroSolicitanteView extends javax.swing.JPanel {
         btnGroup = new javax.swing.ButtonGroup();
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
-        btnSalvar = new javax.swing.JButton();
-        btnCancelar = new javax.swing.JButton();
+        btnSalvarSolicitante = new javax.swing.JButton();
+        btnCancelarSolicitante = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
         tfNomeSolicitante = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
@@ -42,22 +42,23 @@ public class CadastroSolicitanteView extends javax.swing.JPanel {
         ftfCelSolicitante = new javax.swing.JFormattedTextField();
         rbSexoMasculinoSolicitante = new javax.swing.JRadioButton();
         rbSexoFemininoSolicitante = new javax.swing.JRadioButton();
+        jLabel5 = new javax.swing.JLabel();
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Cadastro Solicitante", 2, 0, new java.awt.Font("Tahoma", 0, 14), new java.awt.Color(0, 51, 255))); // NOI18N
 
-        btnSalvar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Save.png"))); // NOI18N
-        btnSalvar.setText("Salvar");
+        btnSalvarSolicitante.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Save.png"))); // NOI18N
+        btnSalvarSolicitante.setText("Salvar");
 
-        btnCancelar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Cancel.png"))); // NOI18N
-        btnCancelar.setText("Cancelar");
+        btnCancelarSolicitante.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/Cancel.png"))); // NOI18N
+        btnCancelarSolicitante.setText("Cancelar");
 
-        jLabel1.setText("Nome*");
+        jLabel1.setText("*Nome:");
 
-        jLabel2.setText("E-mail*");
+        jLabel2.setText("*E-mail:");
 
-        jLabel3.setText("Fone");
+        jLabel3.setText("Telefone:");
 
-        jLabel4.setText("Celular");
+        jLabel4.setText("Celular:");
 
         try {
             ftfFoneSolicitante.setFormatterFactory(new javax.swing.text.DefaultFormatterFactory(new javax.swing.text.MaskFormatter("(##) ####-####")));
@@ -76,19 +77,22 @@ public class CadastroSolicitanteView extends javax.swing.JPanel {
 
         rbSexoFemininoSolicitante.setText("Feminino");
 
+        jLabel5.setText("*Sexo:");
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(btnCancelar)
+                .addComponent(btnCancelarSolicitante)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 299, Short.MAX_VALUE)
-                .addComponent(btnSalvar)
+                .addComponent(btnSalvarSolicitante)
                 .addContainerGap())
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(23, 23, 23)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel5)
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(rbSexoMasculinoSolicitante)
                         .addGap(100, 100, 100)
@@ -110,11 +114,13 @@ public class CadastroSolicitanteView extends javax.swing.JPanel {
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addGap(31, 31, 31)
+                .addContainerGap()
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(tfNomeSolicitante, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jLabel5)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(rbSexoMasculinoSolicitante)
                     .addComponent(rbSexoFemininoSolicitante))
@@ -132,8 +138,8 @@ public class CadastroSolicitanteView extends javax.swing.JPanel {
                     .addComponent(ftfCelSolicitante, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 79, Short.MAX_VALUE)
                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(btnCancelar)
-                    .addComponent(btnSalvar))
+                    .addComponent(btnCancelarSolicitante)
+                    .addComponent(btnSalvarSolicitante))
                 .addContainerGap())
         );
 
@@ -154,41 +160,34 @@ public class CadastroSolicitanteView extends javax.swing.JPanel {
                 .addContainerGap())
         );
 
-        jPanel2.getAccessibleContext().setAccessibleName("Cadastro Solicitante");
-
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 552, Short.MAX_VALUE)
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                    .addGap(0, 9, Short.MAX_VALUE)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGap(0, 10, Short.MAX_VALUE)))
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 1, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 370, Short.MAX_VALUE)
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(layout.createSequentialGroup()
-                    .addGap(0, 7, Short.MAX_VALUE)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGap(0, 7, Short.MAX_VALUE)))
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 1, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnCancelar;
+    private javax.swing.JButton btnCancelarSolicitante;
     private javax.swing.ButtonGroup btnGroup;
-    private javax.swing.JButton btnSalvar;
+    private javax.swing.JButton btnSalvarSolicitante;
     private javax.swing.JFormattedTextField ftfCelSolicitante;
     private javax.swing.JFormattedTextField ftfFoneSolicitante;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JRadioButton rbSexoFemininoSolicitante;
