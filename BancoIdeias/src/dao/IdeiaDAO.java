b7a7684573174b65a0922f19e15118fe0a42670e
Programação <Programao@172.20.116.183>
2015-08-25 20:51:54 -0300

    Ederley

--- BancoIdeias/src/dao/IdeiaDAO.java
+++ BancoIdeias/src/dao/IdeiaDAO.java
@@ -5,10 +5,58 @@
  */
 package dao;
 
+import conexao.ConnectionManager;
+import entidade.Ideia;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Programação
  */
 public class IdeiaDAO {
-    
+
+    public boolean salvar(Ideia ideia) {
+
+        boolean resultado = false;
+
+        try {
+            PreparedStatement stmt = null;
+            Connection conn = ConnectionManager.getConnection();
+
+            String QUERY_INSERT = "insert into ideia (tema, descricao, dt_cadastro) values(?,?,?)";
+            String QUERY_UPDATE = "update ideia set tema = ?, descricao = ?, dt_cadastro = ? where id = ?";
+
+            if (ideia.getId() == null) {
+
+                stmt = conn.prepareStatement(QUERY_INSERT);
+                stmt.setString(1, ideia.getTema());
+                stmt.setString(2, ideia.getDescricao());
+                stmt.setDate(3, ideia.getDtcadastro());
+
+            } else {
+
+                stmt = conn.prepareStatement(QUERY_UPDATE);
+                stmt.setString(1, ideia.getTema());
+                stmt.setString(2, ideia.getDescricao());
+                stmt.setDate(3, ideia.getDtcadastro());
+                //stmt.setInt(4, usuario.getIdUsuario());
+
+            }
+
+            stmt.executeUpdate();
+            conn.close();
+
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso");
+            resultado = true;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            resultado = false;
+        }
+
+        return resultado;
+    }
+
 }
