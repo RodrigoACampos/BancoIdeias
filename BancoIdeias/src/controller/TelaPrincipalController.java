70249adf9ded5b8debd48c30661fe4483b7eb44d
Gustavo Morfim <gustavomorfim@hotmail.com>
2015-08-26 19:05:20 -0300

    corrigido controller principal

--- BancoIdeias/src/controller/TelaPrincipalController.java
+++ BancoIdeias/src/controller/TelaPrincipalController.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
 import java.awt.event.ActionEvent;
@@ -11,6 +6,7 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import view.CadastroAlunoView;
 import view.CadastroIdeiaView;
+import view.IdeiaSolicitanteView;
 import view.CadastroProfessorView;
 import view.CadastroSolicitanteView;
 import view.ConsultaAlunoView;
@@ -19,10 +15,6 @@ import view.ConsultaProfessorView;
 import view.ConsultaSolicitanteView;
 import view.TelaPrincipalView;
 
-/**
- *
- * @author Gustavo
- */
 public class TelaPrincipalController implements ActionListener {
 
     TelaPrincipalView tpv;
