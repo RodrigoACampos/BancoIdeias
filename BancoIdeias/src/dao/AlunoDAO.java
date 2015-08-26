b63a6d510bd94d78dcd1601dac9a8882a616b1bb
vdisoo0046 <vdisoo0046@SNSOO1EP01VM163.saojose.sc.senai.br>
2015-08-26 20:01:21 -0300

    alterado banco dados script

--- BancoIdeias/src/dao/AlunoDAO.java
+++ BancoIdeias/src/dao/AlunoDAO.java
@@ -1,4 +1,8 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package dao;
 
 import conexao.ConnectionManager;
@@ -6,50 +10,142 @@ import entidade.Aluno;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
 
+/**
+ *
+ * @author bruno_balan
+ */
 public class AlunoDAO {
-    
-    public static Aluno inserirCandidato(Aluno aluno) throws SQLException, ClassNotFoundException {
-        Aluno retornoAluno = aluno;
+
+    public boolean salvar(Aluno aluno) {
+        
+        boolean resultado = false;
         try {
+            PreparedStatement stmt = null;
             Connection conn = ConnectionManager.getConnection();
 
-            if (aluno.getId()== null) {
-                PreparedStatement pState = conn.prepareStatement(
-                        "insert into aluno (nome,telefone,email) values (?,?,?)",
-                        Statement.RETURN_GENERATED_KEYS);
-                
-                pState.setString(1, aluno.getNome());
-                pState.setString(2, aluno.getTelefone());
-                pState.setString(3, aluno.getEmail());
+            String QUERY_INSERT = "insert into aluno (nome, email, telefone) values (?, ?, ?)";
+            String QUERY_UPDATE = "update aluno set nome = ?, email = ?, telefone = ? where idaluno = ? ";
 
-                pState.executeUpdate();
-                ResultSet rs = pState.getGeneratedKeys();
-                
-                if (rs.next()) {
-                    aluno.setId(rs.getInt(1));
-                }
-                retornoAluno = aluno;
+            if (aluno.getId() == null) {
+                stmt = conn.prepareStatement(QUERY_INSERT);
+                stmt.setString(1, aluno.getNome());
+                stmt.setString(2, aluno.getEmail());
+                stmt.setString(3, aluno.getTelefone());
             } else {
-
-                PreparedStatement pState = conn.prepareStatement(
-                        "update aluno set nome = ?, telefone = ?,  email = ? where idaluno = ?");
-                pState.setString(1, aluno.getNome());
-                pState.setString(2, aluno.getTelefone());
-                pState.setString(3, aluno.getEmail());
-                pState.setInt(4, aluno.getId());
-                pState.executeUpdate();
-                retornoAluno = aluno;
+                stmt = conn.prepareStatement(QUERY_UPDATE);
+                stmt.setString(1, aluno.getNome());
+                stmt.setString(2, aluno.getEmail());
+                stmt.setString(3, aluno.getTelefone());
+                stmt.setInt(4, aluno.getId());
             }
 
+            stmt.executeUpdate();
             conn.close();
 
-        } catch (SQLException se) {
-            throw new SQLException("Erro ao Cadastrar Novo Aluno: " + se);
+            JOptionPane.showMessageDialog(null, "Salvo com sucesso!");
+            
+            resultado = true;
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            resultado = false;
         }
+
+        return resultado;
+    }
+
+    public boolean deletar(Aluno aluno) {
+
+        boolean resultado = false;
+
+        try {
+            PreparedStatement stmt = null;
+            Connection conn = ConnectionManager.getConnection();
+
+            String QUERY_DELETE = "delete from aluno where idaluno = ?";
+
+            stmt = conn.prepareStatement(QUERY_DELETE);
+            stmt.setInt(1, aluno.getId());
+
+            stmt.executeUpdate();
+            conn.close();
+
+            JOptionPane.showMessageDialog(null, "Deletado com sucesso!");
+            resultado = true;
+
+        } catch (Exception ex) {
+
+            ex.printStackTrace();
+            resultado = false;
+        }
+
+        return resultado;
+    }
+
+    public Aluno GetById(int id) {
+
+        Aluno aluno = new Aluno();
+        
+        try {
+
+            String QUERY_DETALHE = "select * from aluno where idaluno = ?";
+            PreparedStatement stmt = null;
+            Connection conn = ConnectionManager.getConnection();
+
+            ResultSet rs = null;
+
+            stmt = conn.prepareStatement(QUERY_DETALHE);
+            stmt.setInt(1, id);
+
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                aluno = new Aluno();
+                aluno.setId(rs.getInt("idAluno"));
+                aluno.setNome(rs.getString("nome"));
+                aluno.setEmail(rs.getString("email"));
+                aluno.setTelefone(rs.getString("telefone"));
+            }
+            conn.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            aluno = null;
+        }
+        
         return aluno;
     }
-    
+
+    public List<Aluno> listar() {
+        List<Aluno> lista = new ArrayList<Aluno>();
+        try {
+            String QUERY_DETALHE = "select * from aluno";
+            PreparedStatement stmt = null;
+            Connection conn = ConnectionManager.getConnection();
+
+            ResultSet rs = null;
+
+            stmt = conn.prepareStatement(QUERY_DETALHE);
+            rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Aluno aluno = new Aluno();
+                aluno.setId(rs.getInt("idAluno"));
+                aluno.setNome(rs.getString("nome"));
+                aluno.setEmail(rs.getString("email"));
+                aluno.setTelefone(rs.getString("telefone"));
+                lista.add(aluno);
+            }
+            conn.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            return lista;
+        }
+    }
 }
