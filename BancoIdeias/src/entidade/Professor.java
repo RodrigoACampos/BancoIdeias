ff0181e505bbf59fa7b9cf82d2dc4b4cfcb47f66
LUCAS <LUCAS@172.20.116.186>
2015-08-26 19:43:23 -0300

    foi colocado o telefone do professor

--- BancoIdeias/src/entidade/Professor.java
+++ BancoIdeias/src/entidade/Professor.java
@@ -6,6 +6,15 @@ public class Professor {
     private Integer id;
     private String nome;
     private String email; 
+    private String telefone;
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
     
 
     public Integer getId() {
