6fc3585a01ede67a6162452ec289d1c61628ae40
Gustavo Morfim <gustavomorfim@hotmail.com>
2015-08-26 19:10:55 -0300

    corrigido controller principal

--- BancoIdeias/src/view/IdeiaSolicitanteView.java
+++ BancoIdeias/src/view/IdeiaSolicitanteView.java
@@ -14,12 +14,12 @@ import javax.swing.JTextField;
  *
  * @author Gustavo
  */
-public class CadastroIdeiaView extends javax.swing.JDialog {
+public class IdeiaSolicitanteView extends javax.swing.JDialog {
 
     /**
      * Creates new form CadastroIdeiaView1
      */
-    public CadastroIdeiaView(java.awt.Frame parent, boolean modal) {
+    public IdeiaSolicitanteView(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
     }
@@ -166,21 +166,23 @@ public class CadastroIdeiaView extends javax.swing.JDialog {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(CadastroIdeiaView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(IdeiaSolicitanteView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(CadastroIdeiaView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(IdeiaSolicitanteView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(CadastroIdeiaView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(IdeiaSolicitanteView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(CadastroIdeiaView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(IdeiaSolicitanteView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the dialog */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                CadastroIdeiaView dialog = new CadastroIdeiaView(new javax.swing.JFrame(), true);
+                IdeiaSolicitanteView dialog = new IdeiaSolicitanteView(new javax.swing.JFrame(), true);
                 dialog.addWindowListener(new java.awt.event.WindowAdapter() {
                     @Override
                     public void windowClosing(java.awt.event.WindowEvent e) {
