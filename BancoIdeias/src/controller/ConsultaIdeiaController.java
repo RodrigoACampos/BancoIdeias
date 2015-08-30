c49d8fe1bb7b05a34f7ba480f8cbe8a70281f1cc
Gustavo Morfim <gustavomorfim@hotmail.com>
2015-08-29 22:13:14 -0300

    consulta ideia finalizada2

--- BancoIdeias/src/controller/ConsultaIdeiaController.java
+++ BancoIdeias/src/controller/ConsultaIdeiaController.java
@@ -10,6 +10,8 @@ import entidade.Ideia;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.ListSelectionModel;
@@ -22,8 +24,8 @@ import view.ConsultaIdeiaView;
  *
  * @author Gustavo
  */
-public class ConsultaIdeiaController implements ActionListener{
-    
+public class ConsultaIdeiaController implements ActionListener, MouseListener {
+
     Ideia ideia = new Ideia();
     ConsultaIdeiaView consultaIdeiaView;
     IdeiaDAO listaDao = new IdeiaDAO();
@@ -35,7 +37,7 @@ public class ConsultaIdeiaController implements ActionListener{
     public ConsultaIdeiaController(ConsultaIdeiaView consultaIdeiaView) {
         this.consultaIdeiaView = consultaIdeiaView;
     }
-    
+
     public void iniciar() {
         montarEAssinar();
         atualizarTabelaIdeia(ideiaLista);
@@ -45,19 +47,20 @@ public class ConsultaIdeiaController implements ActionListener{
         consultaIdeiaView.getBtnAlterar().addActionListener(this);
         consultaIdeiaView.getBtnConsultar().addActionListener(this);
         consultaIdeiaView.getBtnExcluir().addActionListener(this);
+        consultaIdeiaView.getTbPesquisa().addMouseListener(this);
         consultaIdeiaView.getTbPesquisa().setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 
         ideiaLista = listaDao.listar();
     }
-    
-     public void selecionarDaTabelaIdeias() {
+
+    public void selecionarDaTabelaIdeias() {
         int indice = consultaIdeiaView.getTbPesquisa().getSelectedRow();
         IdeiaTableModel model = (IdeiaTableModel) consultaIdeiaView.getTbPesquisa().getModel();
         ideia = model.getListaIdeias().get(indice);
         System.out.println(ideia.getId());
     }
-     
-      public void atualizarTabelaIdeia(List<Ideia> ideias) {
+
+    public void atualizarTabelaIdeia(List<Ideia> ideias) {
         cellRender.setHorizontalAlignment(SwingConstants.CENTER);
         cellRenderTitle.setHorizontalAlignment(SwingConstants.CENTER);
         cellRenderTitle.setFont(cellRenderTitle.getFont().deriveFont(Font.BOLD)); // Não Funciona, Deveria deixar os Nomes das Colunas em Negrito;
@@ -67,8 +70,8 @@ public class ConsultaIdeiaController implements ActionListener{
         consultaIdeiaView.getTbPesquisa().setModel(modelo);
         consultaIdeiaView.getTbPesquisa().getColumnModel().getColumn(0).setPreferredWidth(10);
         consultaIdeiaView.getTbPesquisa().getColumnModel().getColumn(0).setPreferredWidth(10);
-        consultaIdeiaView.getTbPesquisa().getColumnModel().getColumn(1).setPreferredWidth(300);
-        consultaIdeiaView.getTbPesquisa().getColumnModel().getColumn(2).setPreferredWidth(200);
+        consultaIdeiaView.getTbPesquisa().getColumnModel().getColumn(1).setPreferredWidth(200);
+        consultaIdeiaView.getTbPesquisa().getColumnModel().getColumn(2).setPreferredWidth(300);
 
         consultaIdeiaView.getTbPesquisa().getColumnModel().getColumn(0).setHeaderRenderer(cellRenderTitle);
         consultaIdeiaView.getTbPesquisa().getColumnModel().getColumn(0).setHeaderRenderer(cellRenderTitle);
@@ -77,6 +80,10 @@ public class ConsultaIdeiaController implements ActionListener{
         consultaIdeiaView.getTbPesquisa().getColumnModel().getColumn(1).setCellRenderer(cellRender);
         consultaIdeiaView.getTbPesquisa().getColumnModel().getColumn(2).setHeaderRenderer(cellRenderTitle);
         consultaIdeiaView.getTbPesquisa().getColumnModel().getColumn(2).setCellRenderer(cellRender);
+    }
+    
+     private void atualizarDescricao() {
+         consultaIdeiaView.getTaDescricao().setText(ideia.getDescricao());
     }
 
     @Override
@@ -93,5 +100,30 @@ public class ConsultaIdeiaController implements ActionListener{
             //solicitanteDAO.deletar(solicitante);
         }
     }
-    
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        if (e.getClickCount() == 2) {
+            selecionarDaTabelaIdeias();
+            atualizarDescricao();            
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+   
 }
