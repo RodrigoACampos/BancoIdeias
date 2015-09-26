437f6a9e177ebeb6e6983185a703a622c347e160
ricardo.nene <ricardo.nene@SNSOO1EP01VM089.saojose.sc.senai.br>
2015-09-25 22:01:12 -0300

    Correção no singleton

--- src/conexao/ConnectionManager.java
+++ src/conexao/ConnectionManager.java
@@ -6,26 +6,27 @@ import javax.swing.JOptionPane;
 
 public class ConnectionManager {
 
+    private static Connection conn = null;
+
     public static Connection getConnection() {
 
-        Connection conn = null;
-
-        String STR_DRIVER = "org.gjt.mm.mysql.Driver";
+        if (conn == null) {
+            String STR_DRIVER = "org.gjt.mm.mysql.Driver";
         //String STR_CONEX = "jdbc:mysql://localhost:3306/DBBANCODEIDEIAS";
-        //String USER = "root";
-        //String PASSWORD = "";
-        String STR_CONEX = "jdbc:mysql://mysql07.redehost.com.br:41890/dbBancoIdeias";
-        String USER = "bancoideia";
-        String PASSWORD = "#123456A";
+            //String USER = "root";
+            //String PASSWORD = "";
+            String STR_CONEX = "jdbc:mysql://mysql07.redehost.com.br:41890/dbBancoIdeias";
+            String USER = "bancoideia";
+            String PASSWORD = "#123456A";
 
-        try {
-            Class.forName(STR_DRIVER);
-            conn = DriverManager.getConnection(STR_CONEX, USER, PASSWORD);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(null, "Erro de conexão com o banco de dados!");
+            try {
+                Class.forName(STR_DRIVER);
+                conn = DriverManager.getConnection(STR_CONEX, USER, PASSWORD);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Erro de conexão com o banco de dados!");
+            }
         }
-
         return conn;
     }
 }
